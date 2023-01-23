6c9d004d5fdc3edb3374bc17681e5cef7c901c63
WarLock888 <mr.novozhilov1@gmail.com>
2023-01-23 23:52:41 +0600

    save count, purchases, listfiles and listdirs

--- lesson.py
+++ lesson.py
@@ -5,6 +5,7 @@ import sys
 from victory import start_victory
 from account import start_account
 
+
 def file_dir_copy(filedirname1, filedirname2):
     if os.path.exists(filedirname1):
         if os.path.isdir(filedirname1):
@@ -12,15 +13,18 @@ def file_dir_copy(filedirname1, filedirname2):
         else:
             shutil.copy(filedirname1, filedirname2)  # file
 
+
 def show_dirs(current_dir):
     oslist = os.listdir(current_dir)
     dirlist = list(filter(lambda x: os.path.isdir(x), oslist))
     return dirlist
 
+
 def show_files(current_dir):
     oslist = os.listdir(current_dir)
     filelist = list(filter(lambda x: not os.path.isdir(x), oslist))
     return filelist
+
 
 def main():
     print('Wellcome to console FileManager!')
@@ -33,14 +37,15 @@ def main():
         print('2. удалить (файл/папку)')
         print('3. копировать (файл/папку)')
         print('4. просмотр содержимого рабочей директории')
-        print('5. посмотреть только папки')
-        print('6. посмотреть только файлы')
-        print('7. просмотр информации об операционной системе')
-        print('8. создатель программы')
-        print('9. играть в викторину')
-        print('10. мой банковский счет')
-        print('11. смена рабочей директории')
-        print('12. выход')
+        print('5. сохранить содержимое рабочей директории в файл')
+        print('6. посмотреть только папки')
+        print('7. посмотреть только файлы')
+        print('8. просмотр информации об операционной системе')
+        print('9. создатель программы')
+        print('10. играть в викторину')
+        print('11. мой банковский счет')
+        print('12. смена рабочей директории')
+        print('13. выход')
         print('-------------')
 
         choice = input('Выберите пункт меню ')
@@ -58,34 +63,52 @@ def main():
         elif choice == '3':  # копировать (файл/папку)
             filedirname1 = input('Введите имя файла/папки которую копировать: ')
             filedirname2 = input('Введите имя файла/папки в которую копировать: ')
-            file_dir_copy(filedirname1, filedirname2) # copy file or dir
+            file_dir_copy(filedirname1, filedirname2)  # copy file or dir
         elif choice == '4':  # просмотр содержимого рабочей директории
             oslist = os.listdir(os.getcwd())
             print(oslist)
-        elif choice == '5':  # посмотреть только папки
+        elif choice == '5':  # сохранить содержимое рабочей директории в файл
+            try:
+                with open('listdir.txt', 'w') as f:
+                    # формируем список файлов для записи
+                    filelist = show_files(os.getcwd())
+                    filelist = f'files: {", ".join(filelist)}'
+
+                    # формируем список файлов для записи
+                    dirlist = show_dirs(os.getcwd())
+                    dirlist = f'dirs: {", ".join(dirlist)}'
+
+                    # запись списков в файл
+                    f.write(f'{filelist}\n')
+                    f.write(f'{dirlist}\n')
+                    # print(filelist)
+                    # print(dirlist)
+            except:
+                pass
+        elif choice == '6':  # посмотреть только папки
             dirlist = show_dirs(os.getcwd())
             print(dirlist)
-        elif choice == '6':  # посмотреть только файлы
+        elif choice == '7':  # посмотреть только файлы
             filelist = show_files(os.getcwd())
             print(filelist)
-        elif choice == '7':  # просмотр информации об операционной системе
+        elif choice == '8':  # просмотр информации об операционной системе
             sysinfo = f'My OS is {sys.platform} ({os.name})'
             print(sysinfo)
-        elif choice == '8':  # создатель программы
+        elif choice == '9':  # создатель программы
             name = 'WarLock888'
             info = f'Program created by {name}'
             print(info)
-        elif choice == '9':  # играть в викторину
+        elif choice == '10':  # играть в викторину
             start_victory()
-        elif choice == '10':  # мой банковский счет
+        elif choice == '11':  # мой банковский счет
             start_account()
-        elif choice == '11':  # смена рабочей директории
+        elif choice == '12':  # смена рабочей директории
             dirname = input('Введите имя директории на которую менять рабочую директорию: ')
             if os.path.exists(dirname):
                 os.chdir(dirname)
             else:
                 print(f'Ошибка. Нет такой директории.')
-        elif choice == '12':  # выход
+        elif choice == '13':  # выход
             break
         else:
             print('Неверный пункт меню')
